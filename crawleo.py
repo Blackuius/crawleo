53312c266a0cf28e0cbd80a4a1354a0631db3403
Blackuius <leonardo_ferreirap@hotmail.com>
2021-05-05 15:04:07 -0300

    ajuste de informaçoes

--- crawleo.py
+++ crawleo.py
@@ -4,44 +4,18 @@ print('Iniciando crawleo')
 import requests
 
 from selenium.webdriver import Firefox
-anjely = 'https://anjely.com.br'
 browser = Firefox ()
 browser.set_window_position(0, 0)
 browser.set_window_size(1300, 850)
 
 #entra na url
-browser.get(anjely)
+browser.get('url do site')
 #tira print
-browser.save_screenshot('/home/leonardo/leo/fotossites/anjelyfoto.png')
-print('print anjely')
-##################################
-#novo site
+browser.save_screenshot('/pastadestino.png')
+print('print site')
 from time import sleep
 sleep(1)
-browser.get("http://www.anexi.com.br");
-browser.save_screenshot('/home/leonardo/leo/fotossites/anexifoto.png')
-print('print anexi')
-from time import sleep
-sleep(1)
-##################################
-#novo site
-from time import sleep
-sleep(1)
-browser.get("http://www.auada.adv.br");
-browser.save_screenshot('/home/leonardo/leo/fotossites/auadafoto.png')
-print('print auada')
-from time import sleep
-sleep(1)
-##################################
-#novo site
-from time import sleep
-sleep(1)
-browser.get("http://www.bloqueioimediato.com.br");
-browser.save_screenshot('/home/leonardo/leo/fotossites/bloqueioimediatofoto.png')
-print('print bloqueioimediato')
-from time import sleep
-sleep(1)
-#toda parte sleep para gerenciar a contagem de tempo
+
 #iniciar encerramento do programa
 print('Execucao concluida com sucesso')
 for cont in range(1, -1, -1):
@@ -62,16 +36,14 @@ def send_message(token, chat_id, message):
     except Exception as e:
         print("Erro no sendMessage:", e)
 # token único utilizado para manipular o bot (não deve ser compartilhado)
-token = '1712796177:AAFH8TvzEK1Y40oqdsmsbe8DrZE0whsgjKo'
+token = 'token'
 # id do chat que será enviado as mensagens
-chat_id = '-585311966'
+chat_id = 'id'
 # mensagem
-msg = "Ola anexi prints finalizados, por favor olhar pasta determinada, obrigado"
+msg = "msg"
 # enviar a mensagem
 send_message(token, chat_id, msg)
 
 print('mensagem enviada com sucesso')
 
 #fim
-
-#github att 01
